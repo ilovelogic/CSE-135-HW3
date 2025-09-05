7a3e6f3892ad4474d7bb7efafeca09b5615a0f42
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-09-05 22:27:49 +0000

    Added documentation for routing

--- endpoint.php
+++ endpoint.php
@@ -1,4 +1,22 @@
 <?php
+/*
+HTTP Method | Example Route (Path)                    | Route Description
+--------------------------------------------------------------------------------------------
+GET         | /api.php/reports/user-langs             | Retrieve user language counts report
+GET         | /api.php/reports/spanish-pages          | Retrieve page views by Spanish speakers
+GET         | /api.php/reports/avg-file-serve         | Retrieve average server response time by file
+GET         | /api.php/reports/session-by-width       | Retrieve session counts by screen width
+GET         | /api.php/reports/device-memory          | Retrieve device memory distribution
+
+GET         | /api.php/{resource}                     | Retrieve all entries in resource table (static, activity, performance, apacheLogs)
+GET         | /api.php/{resource}/{id}                | Retrieve specific entry by ID from resource table
+GET         | /api.php/{resource}/{col_1&...&col_n}   | Retrieve specified columns (any delim works) for all entries in resource table
+
+POST        | /api.php/{resource}                     | Add new entry to specified resource table
+PUT         | /api.php/{resource}/{id}                | Update entry by ID in specified resource table
+DELETE      | /api.php/{resource}/{id}                | Delete entry by ID from specified resource table
+*/
+
 require_once __DIR__ . '/src/Controller/AnalyticsController.php';
 
 // Ensures we can use this API in reporting.annekelley.site
