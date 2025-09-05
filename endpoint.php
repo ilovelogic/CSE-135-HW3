79ca3a3372a557c83d800671fb769d75b2b8e104
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-09-05 19:08:06 +0000

    Adjusted for CORS with reporting.annekelley.site

--- endpoint.php
+++ endpoint.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__ . '/src/Controller/AnalyticsController.php';
 
+// Ensures we can use this API in reporting.annekelley.site
+header("Access-Control-Allow-Origin: https://reporting.annekelley.site");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $scriptName = basename($_SERVER['SCRIPT_NAME']); // currently 'endpoint.php'
 $path = preg_replace("#^$scriptName#", '', ltrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
 $path = ltrim($path, '/');  // remove any leading slash remaining
